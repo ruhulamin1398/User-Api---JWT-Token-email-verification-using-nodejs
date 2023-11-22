8d77c33b81cc831357e69f1aad7d192463952f6d
ruhulamin1398 <ruhul.ok@gmail.com>
2023-11-22 20:48:57 +0600

    fix contact model

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("express-async-handler");
-const Contact = require("../models/contactModel");
+const Contact = require("../models/ContactModel");
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access private
